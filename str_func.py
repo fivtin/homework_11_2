4cfcb1e2d9b116fb7f1d5804f8dc13a7098c559a
vitt <fivtin@mail.ru>
2024-01-16 00:28:51 +0300

    create second function and docstrings in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def func_title(value):
+    """docsting to conflict remote state"""
+    return value.title()
+
+
+def new_func_title(value):
+    """processed title function to incoming value"""
     return value.title()
